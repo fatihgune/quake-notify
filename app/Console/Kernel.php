160b7646f9e04acabe6def53c03f56ab59dc3d17
fatih <meechysm@windowslive.com>
2021-02-02 14:11:53 +0000

    update schedule timing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('quake:check')->everyFiveMinutes();
+         $schedule->command('quake:check')->everyMinute();
     }
 
     /**
